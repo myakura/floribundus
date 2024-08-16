de3ba7908a672c5358991eb08cc07e23fe558478
Masataka Yakura <masataka.yakura@gmail.com>
2024-08-16 20:38:00 +0900

    feat: sort tabs within the seleted range

--- floribundus_background.js
+++ floribundus_background.js
@@ -1,9 +1,12 @@
 async function sortSelectedTabsByUrl() {
 	const tabs = await browser.tabs.query({ currentWindow: true, highlighted: true });
 	console.log(`floribundus: tabs obtained: ${tabs}`);
+
+	const leftmostIndex = Math.min(...tabs.map(tab => tab.index));
 	const sortedTabs = tabs.sort((a, b) => a.url.localeCompare(b.url));
+
 	for (let i = 0; i < sortedTabs.length; i++) {
-		await browser.tabs.move(sortedTabs[i].id, { index: i });
+		await browser.tabs.move(sortedTabs[i].id, { index: leftmostIndex + i });
 	}
 }
 
