458545c0e75f3ef28be9bd64ce30845d539c3c04
Masataka Yakura <masataka.yakura@gmail.com>
2024-09-03 03:38:21 +0900

    feat: use `chrome` APIs and callbacks to make it work in Chrome

--- floribundus_background.js
+++ floribundus_background.js
@@ -1,21 +1,22 @@
-async function sortSelectedTabsByUrl() {
-	const tabs = await browser.tabs.query({ currentWindow: true, highlighted: true });
-	console.log(`floribundus: tabs obtained: ${tabs}`);
+function sortSelectedTabsByUrl() {
+	chrome.tabs.query({ currentWindow: true, highlighted: true }, (tabs) => {
+		console.log(`floribundus: tabs obtained: ${tabs}`);
 
-	const leftmostIndex = Math.min(...tabs.map(tab => tab.index));
-	const sortedTabs = tabs.sort((a, b) => a.url.localeCompare(b.url));
+		const leftmostIndex = Math.min(...tabs.map(tab => tab.index));
+		const sortedTabs = tabs.sort((a, b) => a.url.localeCompare(b.url));
 
-	for (let i = 0; i < sortedTabs.length; i++) {
-		await browser.tabs.move(sortedTabs[i].id, { index: leftmostIndex + i });
-	}
+		sortedTabs.forEach((tab, i) => {
+			chrome.tabs.move(tab.id, { index: leftmostIndex + i });
+		});
+	});
 }
 
-browser.browserAction.onClicked.addListener(async () => {
-	await sortSelectedTabsByUrl();
+chrome.browserAction.onClicked.addListener(() => {
+	sortSelectedTabsByUrl();
 });
 
-browser.onCommand.addListener(async (command) => {
-	if (command.name === 'sort-tabs-by-url') {
-		await sortSelectedTabsByUrl();
+chrome.commands.onCommand.addListener((command) => {
+	if (command === 'sort-tabs-by-url') {
+		sortSelectedTabsByUrl();
 	}
 });
