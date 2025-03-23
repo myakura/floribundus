37a1a1521b306f9e0b38c4c1f21b96b8e541a690
Masataka Yakura <masataka.yakura@gmail.com>
2025-03-23 22:38:55 +0900

    feat: use manifest to store extension IDs

--- floribundus_background.js
+++ floribundus_background.js
@@ -74,9 +74,8 @@ async function sortSelectedTabsByUrl() {
 async function fetchTabDates(tabs) {
 	const tabIds = tabs.map((tab) => tab.id);
 
-	const CHROME_EXTENSION_ID = 'mljeinehnapbddnpfpjiipnpdaeeemdi';
-	const FIREFOX_EXTENSION_ID = '{a3d35f84-d68a-456c-8e85-55a25306f580}';
-
+	const manifest = chrome.runtime.getManifest();
+	const { FIREFOX_EXTENSION_ID, CHROME_EXTENSION_ID } = manifest.externals.heliotropium;
 	const extensionId = navigator.userAgent.includes('Firefox')
 		? FIREFOX_EXTENSION_ID
 		: CHROME_EXTENSION_ID;
